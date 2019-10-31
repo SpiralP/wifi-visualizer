630a195065355411593eb5a4e51580aed2daa92e
SpiralP <spiral.palmer@gmail.com>
2019-10-30 19:38:02 -0700

    put ParceljsResponder into parceljs lib

--- src/http_server.rs
+++ src/http_server.rs
@@ -1,10 +1,9 @@
 use crate::{packet_capture::CaptureType, thread, websocket};
 use futures::executor::block_on;
-use http::Response;
-use hyper::Body;
 use log::{debug, info};
+use parceljs::warp::ParceljsResponder;
 use std::net::SocketAddr;
-use warp::{path::FullPath, Filter, Reply};
+use warp::{path::FullPath, Filter};
 
 pub async fn start(addr: SocketAddr, capture_type: CaptureType) {
   info!("starting http/websocket server on http://{}/", addr);
@@ -25,35 +24,8 @@ pub async fn start(addr: SocketAddr, capture_type: CaptureType) {
     })
     .or(warp::path::full().map(|path: FullPath| {
       debug!("http {}", path.as_str());
-      ParceljsResponder { path }
+      ParceljsResponder::new(path)
     }));
 
   warp::serve(routes).bind(addr).await;
-}
-
-struct ParceljsResponder {
-  path: FullPath,
-}
-
-impl Reply for ParceljsResponder {
-  fn into_response(self) -> Response<Body> {
-    let path = self.path.as_str();
-
-    if let Ok(data) = parceljs::get_file(path) {
-      let mut response = Response::builder();
-
-      if let Some(content_type) = parceljs::get_content_type(path) {
-        response.header(&b"Content-Type"[..], content_type);
-      }
-
-      response
-        .body(Body::from(data))
-        .expect("response.body(Body::from(data))")
-    } else {
-      Response::builder()
-        .status(404)
-        .body(Body::empty())
-        .expect("Resposne::builder()..body(empty())")
-    }
-  }
 }
